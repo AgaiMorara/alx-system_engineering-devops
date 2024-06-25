21dada5ef70a47c399ce7c45855ba2c29d937afa
AgaiMorara <corneliusmorara@gmail.com>
2024-06-25 14:47:29 +0300

    lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,11 @@
 #Create a file
-
+#Creates a file in /tmp/school with specific ownership configurations and 
+#with particular content
 file { '/tmp/school':
-     ensure  => file,
-     content => 'I love Puppet',
-     mode    => '0744',
-     owner   => 'www-data'
-     group   => 'www-data',
-}
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data'
+  group   => 'www-data',
+  }
 	  
