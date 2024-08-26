177b82bbfd274a44da94201f3b49fafb793b51ba
AgaiMorara <corneliusmorara@gmail.com>
2024-08-26 16:48:00 +0300

    not cheking

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """Function to Querry Reddit API and return the number of subscribers"""
 
+import json
 import requests
-from sys import argv
-
+import sys
 
 def number_of_subscribers(subreddit):
     """
     Function that queries the Reddit API,
     Returns the number of subscribers
     """
+    headers = {'user-agent': 'Requests'}
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'user-agent': 'request'}
-    data = requests.get(info, allow_redirects=False,
-                        headers=headers)
-    if str(data) == "<Response [200]>":
-        data_json = data.json()
-        return data_json.get("data").get("subscribers")
-    return 0
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False,headers=headers)
+    if r.status_code == 200:
+        return (r.json()["data"]["subscribers"])
+    else:
+        return(0)
