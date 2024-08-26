7f5f1f8e7af2e2d94dd6abf6fdd7671d45bca516
AgaiMorara <corneliusmorara@gmail.com>
2024-08-26 14:07:58 +0300

    subsin subrr

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,14 @@
+#!/usr/bin/python3
+"""
+Function that returns the number of subscribers of a 
+given subreddit
+"""
+
 import requests
 
 def number_of_subscribers(subreddit):
+    """ queries a subbreddit and returns the number of subs
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
 
